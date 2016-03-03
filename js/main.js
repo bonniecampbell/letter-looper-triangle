59a00bfbef33b62120cac1aa905adf160454683f
bonniecampbell <bonnie.jp.campbell@hotmail.com>
2016-03-03 12:37:21 -0500

    Put in the letter looper info and copied each line down so, adding more loopers and subtracting numbers each time

---
+++ js/main.js
@@ -0,0 +1,46 @@
+var letterLooper = prompt('Enter a Letter:');
+
+var i;
+
+for (i = 0; i < 10; i++) {
+  document.write(letterLooper + '<br>');
+
+  for (i = 0; i < 9; i++) {
+    document.write(letterLooper + letterLooper + '<br>');
+  }
+
+  for (i = 0; i < 8; i++) {
+    document.write(letterLooper + letterLooper + letterLooper + '<br>');
+  }
+
+  for (i = 0; i < 7; i++) {
+    document.write(letterLooper + letterLooper + letterLooper + letterLooper + '<br>');
+  }
+
+  for (i = 0; i < 6; i++) {
+    document.write(letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + '<br>');
+  }
+
+  for (i = 0; i < 5; i++) {
+    document.write(letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + '<br>');
+  }
+
+  for (i = 0; i < 4; i++) {
+    document.write(letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + '<br>');
+  }
+
+  for (i = 0; i < 3; i++) {
+    document.write(letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + '<br>');
+  }
+
+  for (i = 0; i < 2; i++) {
+    document.write(letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + '<br>');
+  }
+
+  for (i = 0; i < 1; i++) {
+    document.write(letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + letterLooper + '<br>');
+  }
+
+
+
+}
